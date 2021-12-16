f5fa3cbdc12768e46b21a5ae0dca0793087a303a
worng1234 <54342155+worng1234@users.noreply.github.com>
2021-12-16 10:04:31 +0700

    fix file

--- resources/views/Studentcore/student-info-add-v2.blade.php
+++ resources/views/Studentcore/student-info-add-v2.blade.php
@@ -2214,30 +2214,30 @@
 		<!-- สิ้นสุด Footter -->
 	</div>
 	<!--   Core JS Files   -->
-	<script src="../../assets/js/core/jquery.3.2.1.min.js"></script>
-	<script src="../../assets/js/core/popper.min.js"></script>
-	<script src="../../assets/js/core/bootstrap.min.js"></script>
+	<script src="../assets/js/core/jquery.3.2.1.min.js"></script>
+	<script src="../assets/js/core/popper.min.js"></script>
+	<script src="../assets/js/core/bootstrap.min.js"></script>
 	<!-- jQuery UI -->
-	<script src="../../assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
-	<script src="../../assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
+	<script src="../assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+	<script src="../assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
 	<!-- Moment JS -->
-	<script src="../../assets/js/plugin/moment/moment.min.js"></script>
+	<script src="../assets/js/plugin/moment/moment.min.js"></script>
 	<!-- Bootstrap Toggle -->
-	<script src="../../assets/js/plugin/bootstrap-toggle/bootstrap-toggle.min.js"></script>
+	<script src="../assets/js/plugin/bootstrap-toggle/bootstrap-toggle.min.js"></script>
 	<!-- jQuery Scrollbar -->
-	<script src="../../assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+	<script src="../assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
 	<!-- DateTimePicker -->
-	<script src="../../assets/js/plugin/datepicker/bootstrap-datetimepicker.min.js"></script>
+	<script src="../assets/js/plugin/datepicker/bootstrap-datetimepicker.min.js"></script>
 	<!-- Select2 -->
-	<script src="../../assets/js/plugin/select2/select2.full.min.js"></script>
+	<script src="../assets/js/plugin/select2/select2.full.min.js"></script>
 	<!-- Bootstrap Wizard -->
-	<script src="../../assets/js/plugin/bootstrap-wizard/bootstrapwizard.js"></script>
+	<script src="../assets/js/plugin/bootstrap-wizard/bootstrapwizard.js"></script>
 	<!-- jQuery Validation -->
-	<script src="../../assets/js/plugin/jquery.validate/jquery.validate.min.js"></script>
+	<script src="../assets/js/plugin/jquery.validate/jquery.validate.min.js"></script>
 	<!-- Atlantis JS -->
-	<script src="../../assets/js/atlantis.min.js"></script>
+	<script src="../assets/js/atlantis.min.js"></script>
 	<!-- Atlantis DEMO methods, don't include it in your project! -->
-	<script src="../../assets/js/setting-demo2.js"></script>
+	<script src="../assets/js/setting-demo2.js"></script>
 
 	<script>
 		var $validator = $('.wizard-container form').validate({
